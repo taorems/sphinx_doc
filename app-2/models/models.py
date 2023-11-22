501f7fcbe95fc683a9d35963a1edf2ad36502dba
taorems <sunny.taorem@nov.com>
2023-11-22 10:29:59 +0100

    directive change

--- app-2/models/models.py
+++ app-2/models/models.py
@@ -1,11 +1,15 @@
 class Asset:
     """
-        Assets are the offshore units for which the jacking system data are being collected. 
+    Asset
+    -----
+    Assets are the offshore units for which the jacking system data are being collected. 
     """
     pass
 
 class Operation:
     """
+    Operation
+    ---------
     Operations performed by the asset are called Operations. This includes operations such as leg lowering, leg lifting, platform lowering, platform lifting, preload lowering, preload lifting, etc. 
     The load and distance data for each operation is collected and stored in the database.
     """
